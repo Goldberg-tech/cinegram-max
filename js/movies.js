a2789b38e4f3b8ff206cc1c4e2071357baf6c06a
Nikitos production <nfedoseevdigital@gmail.com>
2026-03-07 17:43:58 +0300

    Update movies.js

--- js/movies.js
+++ js/movies.js
@@ -3,54 +3,27 @@ const playerLink="player.html"
 const movies={
 
 popular:[
-
-{
-title:"Бладшот",
-poster:"https://st.kp.yandex.net/images/film_big/1143242.jpg"
-},
-
-{
-title:"Форсаж",
-poster:"https://st.kp.yandex.net/images/film_big/1178409.jpg"
-},
-
-{
-title:"Джентльмены",
-poster:"https://st.kp.yandex.net/images/film_big/1143242.jpg"
-}
-
+{title:"Джентльмены",poster:"https://st.kp.yandex.net/images/film_big/1143242.jpg"},
+{title:"Форсаж",poster:"https://st.kp.yandex.net/images/film_big/1178409.jpg"},
+{title:"Бладшот",poster:"https://st.kp.yandex.net/images/film_big/1143242.jpg"},
+{title:"Т-34",poster:"https://st.kp.yandex.net/images/film_big/1041611.jpg"}
 ],
 
 series:[
-
-{
-title:"Молодёжка",
-poster:"https://st.kp.yandex.net/images/film_big/799299.jpg"
-},
-
-{
-title:"Кухня",
-poster:"https://st.kp.yandex.net/images/film_big/689066.jpg"
-}
-
+{title:"Молодёжка",poster:"https://st.kp.yandex.net/images/film_big/799299.jpg"},
+{title:"Кухня",poster:"https://st.kp.yandex.net/images/film_big/689066.jpg"},
+{title:"Метод",poster:"https://st.kp.yandex.net/images/film_big/838050.jpg"},
+{title:"Триггер",poster:"https://st.kp.yandex.net/images/film_big/1322324.jpg"}
 ],
 
 comedy:[
-
-{
-title:"Холоп",
-poster:"https://st.kp.yandex.net/images/film_big/1143242.jpg"
-}
-
+{title:"Холоп",poster:"https://st.kp.yandex.net/images/film_big/1143242.jpg"},
+{title:"Ёлки",poster:"https://st.kp.yandex.net/images/film_big/573209.jpg"}
 ],
 
 action:[
-
-{
-title:"Т-34",
-poster:"https://st.kp.yandex.net/images/film_big/1041611.jpg"
-}
-
+{title:"Т-34",poster:"https://st.kp.yandex.net/images/film_big/1041611.jpg"},
+{title:"Майор Гром",poster:"https://st.kp.yandex.net/images/film_big/1108577.jpg"}
 ]
 
 }
