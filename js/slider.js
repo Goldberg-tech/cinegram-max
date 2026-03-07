9f67864756a979cb46e8e19fc29ae7845ac7a78b
Nikitos production <nfedoseevdigital@gmail.com>
2026-03-07 05:12:00 +0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,37 @@
+const hero=document.getElementById("hero")
 
+let i=0
+
+movies.popular.forEach(movie=>{
+
+const img=document.createElement("img")
+
+img.src=movie.poster
+
+img.onclick=()=>{
+
+window.location.href=playerLink
+
+}
+
+hero.appendChild(img)
+
+})
+
+const slides=hero.querySelectorAll("img")
+
+slides.forEach((s,index)=>{
+
+if(index!==0)s.style.display="none"
+
+})
+
+setInterval(()=>{
+
+slides[i].style.display="none"
+
+i=(i+1)%slides.length
+
+slides[i].style.display="block"
+
+},4000)
