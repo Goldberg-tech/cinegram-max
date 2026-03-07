1dbfb7ac334cfb82fe7d55548151c66627acb32a
Nikitos production <nfedoseevdigital@gmail.com>
2026-03-08 01:14:59 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -26,40 +26,22 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 });
 
-}
-
-});
-
-
 // Render movie rows
-
-function renderRow(id,data){
-
-const row=document.getElementById(id)
-
-if(!row || !data) return;
-
-data.forEach(movie=>{
-
-const div=document.createElement("div")
-div.className="poster"
-
-div.innerHTML=`
-<img src="${movie.poster}" alt="${movie.title}">
-`
-
-div.onclick=()=>{
-window.location.href=playerLink
+function renderRow(id, data) {
+  const row = document.getElementById(id);
+  if (!row || !data) return;
+  data.forEach(movie => {
+    const div = document.createElement("div");
+    div.className = "poster";
+    div.innerHTML = `<img src="${movie.poster}" alt="${movie.title}">`;
+    div.onclick = () => {
+      window.location.href = playerLink;
+    };
+    row.appendChild(div);
+  });
 }
 
-row.appendChild(div)
-
-})
-
-}
-
-
-renderRow("popular",movies.popular)
-renderRow("series",movies.series)
-renderRow("comedy",movies.comedy)
-renderRow("action",movies.action)
+renderRow("popular", movies.popular);
+renderRow("series", movies.series);
+renderRow("comedy", movies.comedy);
+renderRow("action", movies.action);
