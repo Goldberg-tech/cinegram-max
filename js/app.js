0cdaafb947153e6250485ae1cb618c89c424ca8d
Nikitos production <nfedoseevdigital@gmail.com>
2026-03-07 05:11:41 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
+function renderRow(id,data){
 
+const row=document.getElementById(id)
+
+data.forEach(movie=>{
+
+const div=document.createElement("div")
+
+div.className="poster"
+
+div.innerHTML=`
+<img src="${movie.poster}">
+`
+
+div.onclick=()=>{
+
+window.location.href=playerLink
+
+}
+
+row.appendChild(div)
+
+})
+
+}
+
+renderRow("popular",movies.popular)
+renderRow("series",movies.series)
+renderRow("comedy",movies.comedy)
+renderRow("action",movies.action)
